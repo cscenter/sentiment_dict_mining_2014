38ad9bdea9292b813bee3106c905f5b06d57bd13
egorbunov <egor-mailbox@narod.ru>
2014-04-11 00:46:20 +0400

    Changes in pair parser

--- Sergej-and-Egor/pairparser/getpairs.py
+++ Sergej-and-Egor/pairparser/getpairs.py
@@ -1,46 +1,50 @@
-# -*- coding: cp1251- -*-
 __author__ = 'Egor Gorbunov'
 
 import sys
 
-pairs_dict = {} #dictionary of dictionaries for pairs of adjectives
+pairs_dict = {}  # dictionary of dictionaries for pairs of adjectives
+pairs_no_neg = {}
+pairs_neg = {}
 
-def add_pair(adj1, adj2, polarity):
+
+def add_pair(dict_to_add, adj1, adj2, polarity):
     """Adds pair of two adjectives into dictionary
-+
+
+    :param dict_to_add: dictionary to precess
     :param adj1: adjective 1
     :param adj2: adjective 2
     :param polarity: = 1 if adjectives conjunct positively
                      = -1 if adj. conjunct negatively
     """
 
-    global pairs_dict
-
-    if adj1 in pairs_dict and adj2 in pairs_dict[adj1]:
-        pairs_dict[adj1][adj2] += polarity
-    elif adj2 in pairs_dict and adj1 in pairs_dict[adj2]:
-        pairs_dict[adj2][adj1] += polarity
-    elif adj1 in pairs_dict:
-        pairs_dict[adj1][adj2] = polarity
-    elif adj2 in pairs_dict:
-        pairs_dict[adj2][adj1] = polarity
+    if adj1 in dict_to_add and adj2 in dict_to_add[adj1]:
+        dict_to_add[adj1][adj2] += polarity
+    elif adj2 in dict_to_add and adj1 in dict_to_add[adj2]:
+        dict_to_add[adj2][adj1] += polarity
+    elif adj1 in dict_to_add:
+        dict_to_add[adj1][adj2] = polarity
+    elif adj2 in dict_to_add:
+        dict_to_add[adj2][adj1] = polarity
     else:
-        pairs_dict[adj1] = {}
-        pairs_dict[adj1][adj2] = polarity
+        dict_to_add[adj1] = {}
+        dict_to_add[adj1][adj2] = polarity
 
-def print_dict(filename):
+
+def print_dict(dict_to_print, filename):
     """Prints pairs_dict into file with given name - filename
     every pair in dictionary is printed in separate line
     %(polarity of pair relation) * frequency% %first adjective% %second adjective%
 
     :param filename:  name of the file to print in
     """
-    global  pairs_dict
     f = open(filename, 'w')
-    for a1 in pairs_dict.keys():
-        for a2 in pairs_dict[a1].keys():
-            f.write(' '.join([str(pairs_dict[a1][a2]), a1, a2, '\n']))
-            #print(pairs_dict[a1][a2])
+    for a1 in dict_to_print.keys():
+        for a2 in dict_to_print[a1].keys():
+            f.write(' '.join([str(dict_to_print[a1][a2]), a1, a2, '\n']))
+            # print(dict_to_print[a1][a2])
+
+    f.close()
+
 
 def get_part_of_speech(string):
     d = {'SPRO': 'pronoun', 'PR': 'pretext', 'ADV': 'adverb', 'S': 'noun', 'V': 'verb'}
@@ -49,6 +53,7 @@ def get_part_of_speech(string):
         return d[spl_str[1]]
     else:
         return ''
+
 
 def get_adjective(string):
     """Checks, if given string stands for adjective
@@ -64,6 +69,7 @@ def get_adjective(string):
         return spl_str[0]
     return ""
 
+
 def is_neg_part(string):
     """Checks, if given string stands for negative particle
 
@@ -77,6 +83,7 @@ def is_neg_part(string):
             return True
     return False
 
+
 def get_conjunction_polarity(string):
     """
     If string describes conjunction, function return it's polarity
@@ -89,10 +96,11 @@ def get_conjunction_polarity(string):
     if spl_str[0] in ['и']:
         return 1
 
-    if spl_str[0] in ['a', 'но', 'зато']:
+    if spl_str[0] in ['a', 'но', 'зато', 'хотя']:
         return -1
 
     return 0
+
 
 def is_separator(string):
     spl_str = string.split('=')
@@ -101,15 +109,17 @@ def is_separator(string):
     else:
         return False
 
+
 def is_end_of_sentence(string):
     spl_str = string.split('_')
     spl_str = list(filter(('').__ne__, spl_str))
     if len(spl_str) > 0:
-        if spl_str[0].count('.') or spl_str[0].count('!') or spl_str[0].count('?')\
-            or spl_str[0].count(';') or spl_str[0] == "\\n\n":
+        if spl_str[0].count('.') or spl_str[0].count('!') or spl_str[0].count('?') \
+                or spl_str[0].count(';') or spl_str[0] == "\\n\n":
             return True
 
     return False
+
 
 def read_next_adjective(file):
     """Input - file, which was processed by mystem.
@@ -124,12 +134,9 @@ def read_next_adjective(file):
               polarity of nearest to adjective conjunction
               )
     """
-    neg_factor = 1
-    neg_index = -1 #index (from current file pos) of negative particle (such as 'не')
+    adj_info = {'adj': "", 'verb_count': 0, 'noun_count': 0, 'neg': 1, 'dist': -1}
+    neg_index = -1  # index (from current file pos) of negative particle (such as 'не')
     i = -1
-    adj = ""
-    verb_count = 0
-    noun_count = 0
     while True:
         i += 1
         my_str = file.readline()
@@ -138,39 +145,38 @@ def read_next_adjective(file):
 
         part = get_part_of_speech(my_str)
         if is_end_of_sentence(my_str):
-            neg_factor = 0
-            i += 1000 #just a big distance
+            adj_info['neg'] = 1
+            i += 1000  # just a big distance
         elif is_neg_part(my_str):
             neg_index = i
         elif part == 'verb':
-            verb_count += 1
+            adj_info['verb_count'] += 1
         elif part == 'noun':
-            noun_count += 1
+            adj_info['noun_count'] += 1
         elif part in ['adverb', 'pretext', 'pronoun'] or is_separator(my_str):
-            if i - neg_index  == 1 and neg_index != -1:
+            if i - neg_index == 1 and neg_index != -1:
                 neg_index += 1
         else:
-            adj = get_adjective(my_str)
-            if adj != "":
+            adj_info['adj'] = get_adjective(my_str)
+            if adj_info['adj'] != "":
                 break
 
-    if i - neg_index  == 1 and neg_index != -1:
-        neg_factor = -1
+    if i - neg_index == 1 and neg_index != -1:
+        adj_info['neg'] = -1
+    adj_info['dist'] = i
 
-    return adj, neg_factor, i, verb_count, noun_count
+    return adj_info
+
 
 def read_conjunction(file):
     """
     :param file: mystemmed file to read from
     :return:
     """
-    verb_count = 0
-    noun_count = 0
-    polarity = 1
+
+    conj_info = {'verb_count': 0, 'noun_count': 0, 'dist': 0, 'adj': "", 'polarity': 1, 'neg': 1}
     distance = 0
-    adjective = ""
     neg_index = -1
-    neg_factor = 1
     while True:
         distance += 1
         s = file.readline()
@@ -179,104 +185,125 @@ def read_conjunction(file):
 
         part = get_part_of_speech(s)
         if is_end_of_sentence(s):
-            polarity = 0
-            distance += 1000 #just a big distance
+            distance += 1000  # just a big distance
             break
         elif is_neg_part(s):
             neg_index = distance
         elif part == 'verb':
-            verb_count += 1
+            conj_info['verb_count'] += 1
         elif part == 'noun':
-            noun_count += 1
+            conj_info['noun_count'] += 1
         elif part in ['adverb', 'pretext', 'pronoun'] or is_separator(s):
-            if distance - neg_index  == 1 and neg_index != -1:
+            if neg_index != -1:
                 neg_index += 1
 
-        adjective = get_adjective(s)
-        if adjective != "":
-            if distance - neg_index  == 1 and neg_index != -1:
-                neg_factor = -1
+        conj_info['adj'] = get_adjective(s)
+        if conj_info['adj'] != "":
+            if distance - neg_index == 1 and neg_index != -1:
+                conj_info['neg'] = -1
             break
 
         p = get_conjunction_polarity(s)
         if p != 0:
-            polarity = p
+            conj_info['polarity'] = p
         if p < 0:
             break
 
-    return polarity, distance, verb_count, noun_count, adjective, neg_factor
+    conj_info['dist'] = distance
+    return conj_info
 
-def analyze_file(in_filename, out_filename):
+
+def print_with_neg(filename):
+    global pairs_dict
+    f = open(filename, 'w')
+    for a1 in pairs_dict.keys():
+        for a2 in pairs_dict[a1].keys():
+            f.write(' '.join([str(pairs_dict[a1][a2]), a1, a2, '\n']))
+            # print(pairs_dict[a1][a2])
+    f.close()
+
+
+def print_merge_neg(filename):
+    global pairs_dict
+
+
+def print_without_neg(filename):
+    global pairs_dict
+
+
+def get_pairs(in_filename):
     """that function parse file and
     get pairs of adjectives with polarity
 
     :param in_filename: file, preprocessed with mystem with "-icln" options
-    :param out_filename: output file with pairs
     """
-    #opening mystem'ed file to parse
+    # opening mystemmed file to parse
     try:
         f = open(in_filename, 'r')
     except IOError:
         print('cannot open file ' + in_filename)
 
-    neg_factor_1 = 1 #negative factor for first adjective
-    neg_factor_2 = 1
-    adj1 = "" #first adjective
-    adj2 = "" #second adjective
-    adj2_distance = 0 #distance between 2-nd adjective and conjunction of first adjective
+    adj1_info = {'adj': ""}
+    adj2_info = {}
+
+    global pairs_dict
+    global pairs_neg
+    global pairs_no_neg
     while True:
         #getting first adjective
-        if adj1 == "":
-            adj1, neg_factor_1, pos1, verb_count1, noun_count1  = read_next_adjective(f)
+        if adj1_info['adj'] == "":
+            adj1_info = read_next_adjective(f)
 
-        if neg_factor_1 == 0:
-            adj1 = ""
-            adj2 = ""
-            continue
-
-        #trying to find conjunction and maybe second adjective if they adjectives go without negative conjunction
+        # trying to find conjunction and maybe second adjective if they adjectives go without negative conjunction
         # (between them can be space, comma, "и")
-        polarity, conj_distance, verb_count2, noun_count2, adj2, neg_factor_2 = read_conjunction(f)
-        if polarity == 0:
-            adj1 = ""
-            adj2 = ""
-            continue
+        conj_info = read_conjunction(f)
 
-        verb_count3 = 0
-        noun_count3 = 0
+        # getting next adjective (second adjective) if it was not found in previous step
+        if conj_info['adj'] == "":
+            adj2_info = read_next_adjective(f)
+        else:
+            adj2_info = {'adj': conj_info['adj'],
+                         'verb_count': 0,
+                         'noun_count': 0,
+                         'neg': conj_info['neg'],
+                         'dist': 0}
 
-        #getting next adjective (second adjective) if it was not found in previous step
-        if adj2 == "":
-            adj2, neg_factor_2, adj2_distance, verb_count3, noun_count3 = read_next_adjective(f)
-
-        if adj1 == "" or adj2 == "":
+        if adj1_info['adj'] == "" or adj2_info['adj'] == "":
             break
 
-        #here we have 2 adjectives, which go successive, but the distance between them can be big
-        #I think, that only number of verbs and nouns matter, and there can only be 2 nouns and verbs btw.
-        #adjectives ('Прекрасное времяпрепровождение - гулять по пляжу, но песок слишком твердый')
-        #adverbs, pronouns, pretext does't matter too. ('Плохое питание, зато, по-моему, очень хороший номер')
-        if verb_count2 <= 1 and noun_count2 == 0 and verb_count3 <= 1 and noun_count3 == 0\
-            and conj_distance <= 5 and adj2_distance <= 5:
-            a1 = adj1
-            a2 = adj2
-            if neg_factor_1 == -1:
-                a1 = ' '.join(['не', adj1])
-            if neg_factor_2 == -1:
-                a2 = ' '.join(['не', adj2])
-            add_pair(a1, a2, polarity)
+        # here we have 2 adjectives, which go successive, but the distance between them can be big.
+        # I think, that only number of verbs and nouns matter, and there can only be 2 nouns and verbs btw.
+        # adjectives ('Прекрасное времяпрепровождение - гулять по пляжу, но песок слишком твердый')
+        # adverbs, pronouns, pretext does not matter too. ('Плохое питание, зато, по-моему, очень хороший номер')
+        if adj2_info['verb_count'] + conj_info['verb_count'] == 0 \
+                and adj2_info['noun_count'] + conj_info['noun_count'] <= 2 \
+                and adj2_info['dist'] + conj_info['dist'] <= 10:
 
-        adj1 = adj2
-        adj2 = ""
-        polarity = neg_factor_2
-        neg_factor_1 = neg_factor_2
-        neg_factor_2 = 1
+            na1 = ""
+            na2 = ""
+            if adj1_info['neg'] == -1:
+                na1 = "не"
+            if adj2_info['neg'] == -1:
+                na2 = "не"
+            add_pair(pairs_dict, ' '.join([na1, adj1_info['adj']]).strip(),  # strip is used to delete first space
+                     ' '.join([na2, adj2_info['adj']]).strip(),
+                     conj_info['polarity'])
+
+            add_pair(pairs_neg, ''.join([na1, adj1_info['adj']]),
+                     ''.join([na2, adj2_info['adj']]), conj_info['polarity'])
+
+            add_pair(pairs_no_neg, adj1_info['adj'], adj2_info['adj'],
+                     adj2_info['neg'] * adj1_info['neg'] * conj_info['polarity'])
+
+        adj1_info = adj2_info
 
     f.close()
-    print_dict(out_filename)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        sys.stderr.write('=(')
+    if len(sys.argv) < 2:
+        sys.stderr.write('ERROR: Bad arguments')
         sys.exit(1)
-    analyze_file(sys.argv[1], sys.argv[2])
+    get_pairs(sys.argv[1])
+    print_dict(pairs_no_neg, "no_neg_pairs.txt")
+    print_dict(pairs_neg, "neg_pairs.txt")
