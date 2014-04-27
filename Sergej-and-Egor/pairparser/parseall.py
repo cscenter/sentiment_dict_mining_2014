7402b72e6cccdc42aa9ff579a5b80ccbcc031178
egorbunov <egor-mailbox@narod.ru>
2014-04-27 22:45:32 +0400

    Script for escaping from negative prefixes

--- Sergej-and-Egor/pairparser/parseall.py
+++ Sergej-and-Egor/pairparser/parseall.py
@@ -8,5 +8,4 @@ for i in range(1, 27):
     print(i)
     pair_parser.get_pairs('/home/egor/PycharmProjects/SplitBigFile/mystemmed/mfile' + str(i) + '.txt')
 
-pair_parser.print_pairs_neg('all_pairs_neg(1).txt')
-pair_parser.print_pairs_no_neg('all_pairs_no_neg(1).txt')
+pair_parser.print_pairs_no_neg('results/all_pairs_no_neg(2).txt')
