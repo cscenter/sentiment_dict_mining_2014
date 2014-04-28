1abbff0820b9d6d02a6e0649550e21475b49da43
egorbunov <egor-mailbox@narod.ru>
2014-04-29 00:04:49 +0400

    500 dict creation sh file

---
+++ testing/merge.sh
@@ -0,0 +1,14 @@
+python3 recall/merge_2_sent_files.py merge_results/init_files/neg_1_alina.txt merge_results/init_files/neg_1_yura.txt merge_results/2merged/neg_1.txt merge_results/differences/dif_neg_1.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/neg_2_alina.txt merge_results/init_files/neg_2_edgar.txt merge_results/2merged/neg_2.txt merge_results/differences/dif_neg_2.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/neg_3_edgar.txt merge_results/init_files/neg_3_sergej.txt merge_results/2merged/neg_3.txt merge_results/differences/dif_neg_3.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/neg_4_sergej.txt merge_results/init_files/neg_4_egor.txt merge_results/2merged/neg_4.txt merge_results/differences/dif_neg_4.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/neg_5_egor.txt merge_results/init_files/neg_5_yura.txt merge_results/2merged/neg_5.txt merge_results/differences/dif_neg_5.txt
+
+python3 recall/merge_2_sent_files.py merge_results/init_files/pos_1_alina.txt merge_results/init_files/pos_1_yura.txt merge_results/2merged/pos_1.txt merge_results/differences/dif_pos_1.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/pos_2_alina.txt merge_results/init_files/pos_2_edgar.txt merge_results/2merged/pos_2.txt merge_results/differences/dif_pos_2.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/pos_3_edgar.txt merge_results/init_files/pos_3_sergej.txt merge_results/2merged/pos_3.txt merge_results/differences/dif_pos_3.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/pos_4_sergej.txt merge_results/init_files/pos_4_egor.txt merge_results/2merged/pos_4.txt merge_results/differences/dif_pos_4.txt
+python3 recall/merge_2_sent_files.py merge_results/init_files/pos_5_egor.txt merge_results/init_files/pos_5_yura.txt merge_results/2merged/pos_5.txt merge_results/differences/dif_pos_5.txt
+
+python3 recall/merge_5_sent_files.py merge_results/2merged/neg_1.txt merge_results/2merged/neg_2.txt merge_results/2merged/neg_3.txt merge_results/2merged/neg_4.txt merge_results/2merged/neg_5.txt merge_results/500_neg.txt
+python3 recall/merge_5_sent_files.py merge_results/2merged/pos_1.txt merge_results/2merged/pos_2.txt merge_results/2merged/pos_3.txt merge_results/2merged/pos_4.txt merge_results/2merged/pos_5.txt merge_results/500_pos.txt
