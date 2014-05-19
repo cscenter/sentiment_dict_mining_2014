15318e71d791b4ce2ae69aaddd9d640a29452868
egorbunov <egor-mailbox@narod.ru>
2014-05-19 15:15:27 +0400

    min cut improve

--- Sergej-and-Egor/min_cut/min_cut.py
+++ Sergej-and-Egor/min_cut/min_cut.py
@@ -6,13 +6,12 @@ filename = '../pairparser/results/en_pairs(7).txt'
 print(filename)
 coefficient = 3
 
-
 word_dict = {}
 add_dict = {}
 
 f = open('bad.txt', 'r', encoding="utf-8")
 for s in f:
-    print(s.split(' ')[0])
+    # print(s.split(' ')[0])
     add_dict[s.split(' ')[0]] = 1
 
 f = open(filename, 'r', encoding="utf-8")
@@ -59,20 +58,24 @@ largest_label = label_largest_component(pairs_graph)
 
 mc, part = min_cut(pairs_graph, edge_weights)
 
-print(mc)
-print(part)
+print("Cut value = " + str(mc))
 
 f1 = open('first_part.txt', 'w', encoding="utf-8")
 f2 = open('sec_part.txt', 'w', encoding="utf-8")
 f3 = open('add_comp.txt', 'w', encoding="utf-8")
+
+count1 = 0
+count2 = 0
 for key in word_dict.keys():
     # print(key)
     # print(part[pairs_graph.vertex(word_dict[key])])
     v = pairs_graph.vertex(word_dict[key])
-    if largest_label[v] == 1:
+    if largest_label[v] == 1:  # if only in largest component
         if part[v] == 0:
+            count1 += 1
             f1.write(key + '\n')
         else:
+            count2 += 1
             f2.write(key + '\n')
     else:
         f3.write(key + ' ' + str(components_label[0][v]) + ' ' + str(part[v]) + '\n')
@@ -81,21 +84,89 @@ f1.close()
 f2.close()
 f3.close()
 
-v = pairs_graph.vertex(word_dict['отвратительный'])
-topos = 0
-toneg = 0
-for w in v.out_neighbours():
-    if part[w] == 0:
-        topos += edge_weights[pairs_graph.edge(v, w)]
-    else:
-        toneg += edge_weights[pairs_graph.edge(v, w)]
+# position = arf_layout(pairs_graph, max_iter=3)
+# graph_draw(pairs_graph, pos=position, vertex_text=ver_names, edge_text=edge_weights, vertex_size=5, vertex_font_size=10,
+#          edge_font_size=15, vertex_fill_color=part)
 
-print(topos)
-print(toneg)
+# improving cut
+dists = pairs_graph.new_vertex_property("int")
+pos = 0
+neg = 1
+polarity = ["pos", "neg"]
+if count1 < count2:
+    pos, neg = neg, pos
+    polarity = ["neg", "pos"]
+
+bad_vertices = {}
+for v in pairs_graph.vertices():
+    dists[v] = 0
+    for w in v.out_neighbours():
+        if part[w] == pos:
+            dists[v] += edge_weights[pairs_graph.edge(v, w)]
+        else:
+            dists[v] -= edge_weights[pairs_graph.edge(v, w)]
+    # print(ver_names[v] + " is in " + polarity[part[v]] + " -> " + str(dists[v]))
+    if part[v] == pos and dists[v] < 0:
+        bad_vertices[v] = dists[v]
+    elif part[v] == neg and dists[v] > 0:
+        bad_vertices[v] = -dists[v]
+    else:
+        bad_vertices[v] = 0
+
+worst_vertex = min(bad_vertices, key=bad_vertices.get)
+
+max_iter = 1000
+i = 0
+while bad_vertices[worst_vertex] != 0 and i < max_iter:
+    i += 1
+    print(i)
+    # print(bad_vertices)
+    part[worst_vertex] = 1 - part[worst_vertex]
+    bad_vertices[worst_vertex] = 0
+    for v in worst_vertex.out_neighbours():
+        dists[v] = 0
+        for w in v.out_neighbours():
+            if part[w] == pos:
+                dists[v] += edge_weights[pairs_graph.edge(v, w)]
+            else:
+                dists[v] -= edge_weights[pairs_graph.edge(v, w)]
+        if part[v] == pos and dists[v] < 0:
+            bad_vertices[v] = dists[v]
+        elif part[v] == neg and dists[v] > 0:
+            bad_vertices[v] = -dists[v]
+        else:
+            bad_vertices[v] = 0
+    worst_vertex = min(bad_vertices, key=bad_vertices.get)
+
+cut_value = 0
+for e in pairs_graph.edges():
+    if part[e.source()] != part[e.target()]:
+        cut_value += edge_weights[e]
+
+print("New cut value = " + str(cut_value))
+
+f1 = open('new2_first_part.txt', 'w', encoding="utf-8")
+f2 = open('new2_sec_part.txt', 'w', encoding="utf-8")
+
+count1 = 0
+count2 = 0
+for key in word_dict.keys():
+    # print(key)
+    # print(part[pairs_graph.vertex(word_dict[key])])
+    v = pairs_graph.vertex(word_dict[key])
+    if largest_label[v] == 1:  # if only in largest component
+        if part[v] == 0:
+            count1 += 1
+            f1.write(key + '\n')
+        else:
+            count2 += 1
+            f2.write(key + '\n')
+f1.close()
+f2.close()
 
 #pos = arf_layout(pairs_graph, max_iter=3)
-#graph_draw(pairs_graph, pos=pos, vertex_text=ver_names, edge_text=edge_weights, vertex_size=1, vertex_font_size=2,
-#           edge_font_size=4, output="gr.pdf", vertex_fill_color=part)
+#graph_draw(pairs_graph, pos=pos, vertex_text=ver_names, edge_text=edge_weights, vertex_size=5,  vertex_font_size=10,
+#           edge_font_size=15, vertex_fill_color=part)
 
 
 
